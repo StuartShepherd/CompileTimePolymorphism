b1de90ea79a77546b54d2a53b3f4a823f879c997
Stuart Shepherd <stuartshepherd@gmail.com>
2020-03-05 20:12:50 +1100

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CompileTimePolymorphism
+{
+    public class Calculate
+    {
+        public void AddNumbers(int a, int b)
+        {
+            Console.WriteLine("a + b = {0}", a + b);
+        }
+
+        public void AddNumbers(int a, int b, int c)
+        {
+            Console.WriteLine("a + b + c = {0}", a + b + c);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Compile Time Polymorphism");
+            Console.WriteLine("Polymorphism provides an ability for the classes to implement different methods that are called through the same name and it also provides an ability to invoke the methods of a derived class through base class reference during runtime based on our requirements.");
+            Console.WriteLine("Compile Time Polymorphism means defining multiple methods with the same name but with different parameters. By using compile-time polymorphism, we can perform different tasks with the same method name by passing different parameters.");
+            Console.WriteLine("");
+
+            Calculate c = new Calculate();
+            c.AddNumbers(1, 2);
+            c.AddNumbers(1, 2, 3);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Compile Time Polymorphism Example
+Polymorphism provides an ability for the classes to implement different methods that are called through the same name and it also provides an ability to invoke the methods of a derived class through base class reference during runtime based on our requirements.
+
+Compile Time Polymorphism means defining multiple methods with the same name but with different parameters. By using compile-time polymorphism, we can perform different tasks with the same method name by passing different parameters.
+
+## Getting Started
+
+### Prerequisites
+
+[.NET Core 3.1 SDK or later](https://dotnet.microsoft.com/download/dotnet-core/3.1)
